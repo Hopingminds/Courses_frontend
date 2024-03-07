31e0fdf78115881df02e8dd11f1199f0f3c0763a
Davinder9297 <davindergiri765@gmail.com>
2024-03-07 17:06:58 +0530

    updated

--- src/Components/CourseFeatures/CourseFeatures.js
+++ src/Components/CourseFeatures/CourseFeatures.js
@@ -3,18 +3,18 @@ import './coursefeatures.css';
 function CourseFeatures() {
     return (
         <div className='CF-container '>
-            <div className='CF-heading'>
+            <div className='CF-heading text-5xl'>
                 <h3>Our Feature Courses</h3>
             </div>
             <div className='CF-hero'>
                 <div className='CF-box'>
-                    <div className='CF-img'>
+                    <div className='CF-img 2xl:h-[100%] 2xl:w-[40%]'>
                         <img className='w-[100%] h-[100%]' src='../img/featuresImage.png' alt='person-with-laptop'></img>
                     </div>
-                    <div className='CF-content space-y-3'>
+                    <div className='CF-content space-y-3 ml-4 w-[73%]'>
                         <div className='CF-row1'>
-                            <div className='CF-health-box'>
-                                <p className='CF-health-para'>Health & Fitness</p>
+                            <div className='w-auto'>
+                                <p className='CF-health-para bg-[#E1F7E3]'>Health & Fitness</p>
                             </div>
                             <div className='CF-price'>
                                 <p className='CF-org-price'>₹4999</p>
@@ -50,16 +50,15 @@ function CourseFeatures() {
                             </div>
                         </div>
                     </div>
-                </div>
-                
+                </div>                
                 <div className='CF-box'>
-                    <div className='CF-img'>
-                        <img src='../img/featuresImage.png' alt='person-with-laptop'></img>
+                    <div className='CF-img 2xl:h-[100%] 2xl:w-[40%]'>
+                        <img className='w-[100%] h-[100%]' src='../img/featuresImage.png' alt='person-with-laptop'></img>
                     </div>
-                    <div className='CF-content space-y-3'>
+                    <div className='CF-content space-y-3 ml-4 w-[73%]'>
                         <div className='CF-row1'>
-                            <div className='CF-health-box'>
-                                <p className='CF-health-para'>Health & Fitness</p>
+                            <div className='w-auto'>
+                                <p className='CF-health-para bg-[#E1F7E3]'>Health & Fitness</p>
                             </div>
                             <div className='CF-price'>
                                 <p className='CF-org-price'>₹4999</p>
@@ -95,8 +94,95 @@ function CourseFeatures() {
                             </div>
                         </div>
                     </div>
-                </div>
-                
+                </div>                
+                <div className='CF-box'>
+                    <div className='CF-img 2xl:h-[100%] 2xl:w-[40%]'>
+                        <img className='w-[100%] h-[100%]' src='../img/featuresImage.png' alt='person-with-laptop'></img>
+                    </div>
+                    <div className='CF-content space-y-3 ml-4 w-[73%]'>
+                        <div className='CF-row1'>
+                            <div className='w-auto'>
+                                <p className='CF-health-para bg-[#E1F7E3]'>Health & Fitness</p>
+                            </div>
+                            <div className='CF-price'>
+                                <p className='CF-org-price'>₹4999</p>
+                            </div>
+                        </div>
+                        <div className='CF-row2'>
+                            <div className='CF-row2-para'>Investing In Stocks the complete course!(13 H...</div>
+                        </div>
+                        <div className='CF-row3'>
+                            <div className='CF-row3-left'>
+                                <img src='../img/featuresImage2.png' alt='small-img'></img>
+                                <p className='CF-row3-name'>Kevin Gilbert</p>
+                            </div>
+                            <div className='CF-row3-right'>
+                                <div className='CF-row3-star'>
+                                    <img src="../Icons/Star.svg" alt="star"></img>
+                                </div>
+                                <p className='CF-row3-rating'>5.0 <span>(357,914)</span></p>
+                            </div>
+                        </div>
+                        <div className='CF-row4'>
+                            <div className='CF-row4-user'>
+                                <img src="../Icons/User.svg" alt="User"></img>
+                                <p className='CF-row4-username'>265.7K <span> students</span></p>
+                            </div>
+                            <div className='CF-row4-hours'>
+                                <img src="../Icons/Clock.svg" alt="Clock"></img>
+                                <p className='CF-row4-clock'>6 hour</p>
+                            </div>
+                            <div className='CF-row4-level'>
+                                <img src="../Icons/bar-chart.svg" alt="bar-chart"></img>
+                                <p className='CF-row4-levels'>Beginner</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+                <div className='CF-box'>
+                    <div className='CF-img 2xl:h-[100%] 2xl:w-[40%]'>
+                        <img className='w-[100%] h-[100%]' src='../img/featuresImage.png' alt='person-with-laptop'></img>
+                    </div>
+                    <div className='CF-content space-y-3 ml-4 w-[73%]'>
+                        <div className='CF-row1'>
+                            <div className='w-auto'>
+                                <p className='CF-health-para bg-[#E1F7E3]'>Health & Fitness</p>
+                            </div>
+                            <div className='CF-price'>
+                                <p className='CF-org-price'>₹4999</p>
+                            </div>
+                        </div>
+                        <div className='CF-row2'>
+                            <div className='CF-row2-para'>Investing In Stocks the complete course!(13 H...</div>
+                        </div>
+                        <div className='CF-row3'>
+                            <div className='CF-row3-left'>
+                                <img src='../img/featuresImage2.png' alt='small-img'></img>
+                                <p className='CF-row3-name'>Kevin Gilbert</p>
+                            </div>
+                            <div className='CF-row3-right'>
+                                <div className='CF-row3-star'>
+                                    <img src="../Icons/Star.svg" alt="star"></img>
+                                </div>
+                                <p className='CF-row3-rating'>5.0 <span>(357,914)</span></p>
+                            </div>
+                        </div>
+                        <div className='CF-row4'>
+                            <div className='CF-row4-user'>
+                                <img src="../Icons/User.svg" alt="User"></img>
+                                <p className='CF-row4-username'>265.7K <span> students</span></p>
+                            </div>
+                            <div className='CF-row4-hours'>
+                                <img src="../Icons/Clock.svg" alt="Clock"></img>
+                                <p className='CF-row4-clock'>6 hour</p>
+                            </div>
+                            <div className='CF-row4-level'>
+                                <img src="../Icons/bar-chart.svg" alt="bar-chart"></img>
+                                <p className='CF-row4-levels'>Beginner</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>                
             </div>
         </div>
     );
