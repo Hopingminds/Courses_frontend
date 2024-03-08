af83b8b55057eddfeb01bd7aac8344b6ea90651e
anujHopingminds <anuj@hoopinminds.com>
2024-03-08 15:22:48 +0530

    resovlve conflit

--- src/Components/Category/index.js
+++ src/Components/Category/index.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
 import { Row, Col } from "react-bootstrap";
 export default function Category() {
     return (
@@ -87,71 +86,4 @@ export default function Category() {
             </Row>
         </>
     )
-=======
-
-export default function Category(){
-    return(<>
-    <div className="text-center text-[50px] font-semibold font-outfit mt-16">Top  industry courses in demand</div>
-    <div className="flex justify-center w-full ">
-    <div className="flex flex-wrap w-[90vw]  mt-10  2xl:justify-between pl-[6%]">
-            <Link to='/courses?category=full-stack-developer' className="flex h-24 w-[25%] 2xl:w-[20%] m-4  bg-[#EBEBFF] items-center">
-                    <div ><img className="mix-blend-multiply" src="/fsd.png"/></div>
-                    <div>
-                        <div className="font-semibold">Full Stack Development</div>
-                        <div className="text-sm text-[#6E7485]">63,476 Courses</div>
-                    </div>
-            </Link>
-            <Link to='/courses?category=data-science' className="flex h-24 w-[25%] 2xl:w-[20%] m-4  bg-[#FFF0F0] items-center">
-                    <div ><img className="mix-blend-multiply" src="/ds.png"/></div>
-                    <div>
-                        <div className="font-semibold">Data Science</div>
-                        <div className="text-sm text-[#6E7485]">63,476 Courses</div>
-                    </div>
-            </Link>
-            <Link to='/courses?category=electric-vehicle-design' className="flex h-24 w-[25%] 2xl:w-[20%] m-4  bg-[#EBEBFF] items-center">
-                    <div className=""><img className="mix-blend-multiply" src="/evd.png"/></div>
-                    <div>
-                        <div className="font-semibold">Electric Vehicle Design</div>
-                        <div className="text-sm text-[#6E7485]">63,476 Courses</div>
-                    </div>
-            </Link>
-            <Link to='/courses?category=ai&ml' className="flex h-24 w-[25%] 2xl:w-[20%] m-4  bg-[#FFF0F0] items-center">
-                    <div className="h-20 w-20  flex justify-center items-center"><img className="mix-blend-multiply" src="/ai.png"/></div>
-                    <div>
-                        <div className="font-semibold">AI & ML</div>
-                        <div className="text-sm text-[#6E7485]">63,476 Courses</div>
-                    </div>
-            </Link>                    
-    <Link to='/courses?category=design' className="flex h-24 w-[25%] 2xl:w-[20%] m-4  bg-[#FFF0F0] items-center">
-                    <div ><img className="mix-blend-multiply" src="/design.png"/></div>
-                    <div>
-                        <div className="font-semibold">Design</div>
-                        <div className="text-sm text-[#6E7485]">63,476 Courses</div>
-                    </div>
-            </Link>
-            <Link to='/courses?category=hydro-carbon' className="flex h-24 w-[25%] 2xl:w-[20%] m-4  bg-[#E1F7E3] items-center">
-                    <div ><img className="mix-blend-multiply" src="/carbon.png"/></div>
-                    <div>
-                        <div className="font-semibold">Hydro Carbon</div>
-                        <div className="text-sm text-[#6E7485]">63,476 Courses</div>
-                    </div>
-            </Link>
-            <Link to='/courses?category=cyber-security' className="flex h-24 w-[25%] 2xl:w-[20%] m-4  bg-[#FFF0F0] items-center">
-                    <div ><img className="mix-blend-multiply" src="/cyber.png"/></div>
-                    <div>
-                        <div className="font-semibold">Cyber Security</div>
-                        <div className="text-sm text-[#6E7485]">63,476 Courses</div>
-                    </div>
-            </Link>           
-            <Link to='/courses?category=management' className="flex h-24 w-[25%] 2xl:w-[20%] m-4  bg-[#EBEBFF] items-center">
-                    <div className="ml-2"><img  className="mix-blend-multiply w-14 h-12" src="/structur.png"/></div>
-                    <div className="ml-4">
-                        <div className="font-semibold">Management</div>
-                        <div className="text-sm text-[#6E7485]">63,476 Courses</div>
-                    </div>
-            </Link>           
-    </div>
-    </div>
-    </>)
->>>>>>> 3c14e31c699c22581921be285ef56a265b991350
 } 
--- src/Components/companies/Company.js
+++ src/Components/companies/Company.js
@@ -2,34 +2,12 @@ import { Col, Row } from "react-bootstrap";
 
 export default function Companies() {
     return (
-<<<<<<< HEAD
         <Row className="header">
             <Col>
                 <div>
                     <div>
                         <div>
                             <h2 className='font-[outfit] text-center font-semibold text-2xl'>150+ Companies are waiting for you</h2>
-=======
-        <div className="mt-16">
-            <div>
-                <div>
-                    <h2 className='font-[outfit] text-center font-semibold text-5xl'>150+ Companies are waiting for you</h2>
-                </div>
-                <div>
-                    <p className='font-[montserrat] text-center py-3 px-28'>We'll help you get hired. Our team will work with you throughout the program and after graduation to help you embellish your portfolio, practice interviewing and land a job that enhances your skills and accelerates your growth.</p>
-                </div>
-            </div>
-            <div>
-                <div className="h-[65px] my-5 mx-[40px] overflow-hidden ">
-                    <div className='flex w-max '>
-                        <div className='flex flex-row mt-[5px] animate-[scroll_20s_linear_infinite] '>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/paytm.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/sharechat.png' alt='sharechat'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/google.png' alt='google'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/ajio.png' alt='ajio'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/indmoney.png' alt='indmoney'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/whatfix.png' alt='ajio'></img>
->>>>>>> 3c14e31c699c22581921be285ef56a265b991350
                         </div>
                         <div>
                             <p className='font-[montserrat] text-center py-3 px-28'>We'll help you get hired. Our team will work with you throughout the program and after graduation to help you embellish your portfolio, practice interviewing and land a job that enhances your skills and accelerates your growth.</p>
@@ -100,54 +78,7 @@ export default function Companies() {
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
             </Col>
         </Row>
-=======
-                <div className="h-[65px] my-5 mx-[70px] overflow-hidden">
-                    <div className='flex w-max'>
-                        <div className='flex flex-row mt-[5px] animate-[scroll2_17s_linear_infinite]'>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/wipro.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/revv.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/infosys.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/sharechat.png' alt='sharechat'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/uolo.png' alt='google'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/ola.png' alt='ajio'></img>
-                        </div>
-                        <div className='flex flex-row mt-[5px] animate-[scroll2_17s_linear_infinite] left-[-100%] relative'>
-                            <img className='flex px-4 w-[170px] h-[70px]' src='../img/wipro.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/revv.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/infosys.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/sharechat.png' alt='sharechat'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/uolo.png' alt='google'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/ola.png' alt='ajio'></img>
-                        </div>
-                    </div>
-                </div>
-                <div className="h-[65px] my-5 mx-[40px] overflow-hidden">
-                    <div className='flex w-max'>
-                        <div className='flex flex-row mt-[5px] animate-[scroll_27.8s_linear_infinite]'>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/capegimini.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/tcs.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/swiggy.png' alt='sharechat'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/accenture.png' alt='google'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/whatfix.png' alt='ajio'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/ibm.png' alt='ajio'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/whitehat.png' alt='ajio'></img>
-                        </div>
-                        <div className='flex flex-row mt-[5px] animate-[scroll_27.8s_linear_infinite]'>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/capegimini.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/tcs.png' alt='paytm'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/swiggy.png' alt='sharechat'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/accenture.png' alt='google'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/whatfix.png' alt='ajio'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/ibm.png' alt='ajio'></img>
-                            <img className='px-4 w-[185px] h-[70px]' src='../img/whitehat.png' alt='ajio'></img>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
->>>>>>> 3c14e31c699c22581921be285ef56a265b991350
     );
 }
