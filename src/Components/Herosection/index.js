c7034b06eccee00b53443729b2de7f840d973356
LakshitaAgrawal <lakshitagoel888@gmail.com>
2024-03-16 11:24:46 +0530

    header

--- src/Components/Herosection/index.js
+++ src/Components/Herosection/index.js
@@ -8,10 +8,10 @@ export default function Herosection() {
     const navigate = useNavigate()
     return (<>
         <div className="border bg-[#E2FFF1] h-[87.5vh] hero w-full flex font-pop justify-center items-center px-[3%]">
-            <div className='w-[45%] space-y-5'>
-                <div className='text-5xl font-bold'>Studying Online is now much easier</div>
-                <div className='font-nu text-[20px]'>TOTC is an interesting platform that will teach you in more an interactive way</div>
-                <button className='px-6 py-3 text-white bg-[#1DBF73] rounded-full ' onClick={() => navigate('/register')}>Join for free</button>
+            <div className='w-[45%] space-y-12'>
+                <div className='text-[54px] font-bold w-[90%]'>Studying Online is now much easier</div>
+                <div className='font-nu text-[24px] w-[70%]'>TOTC is an interesting platform that will teach you in more an interactive way</div>
+                <button className='px-6 py-3 text-white bg-[#1DBF73] rounded-full text-[24px]' onClick={() => navigate('/register')}>Join for free</button>
             </div>
             <div className='w-[50%] flex justify-center items-center  relative text-[#545567]'>
                 <div className='h-[90%] w-[90%] herobox'>
@@ -19,27 +19,27 @@ export default function Herosection() {
 
                     {/* <img  className='w-full h-full ' src='/tiger.webp' /> */}
                 </div>
-                <div className='flex absolute -top-8 left-10  bg-white py-2 px-4 space-x-3 items-center font-nu rounded-lg'>
+                <div className='flex absolute -top-12 left-12 w-[29%] bg-[#FFFFFFCC] py-4 pl-5 gap-6 items-center font-nu rounded-xl' style={{ backdropFilter: 'blur(8px)' }}>
                     <Cal />
-                    <div className='flex flex-col '>
+                    <div className='flex flex-col gap-1'>
                         <div className='font-semibold text-[18px]'>250k</div>
-                        <div>Students</div>
+                        <div>Assisted Students</div>
                     </div>
                 </div>
-                <div className='flex absolute top-[50%] left-0  h-auto bg-white  space-x-2 items-start font-nu py-2 px-3 rounded-lg'>
+                <div className='flex absolute top-[40%] left-0 w-[35%] h-auto bg-[#FFFFFFCC] gap-4 items-start font-nu py-5 px-4 rounded-xl' style={{ backdropFilter: 'blur(8px)' }} >
                     <img src='/dp.png' />
-                    <div className='flex flex-col space-y-1'>
+                    <div className='flex flex-col gap-1'>
                         <div className='font-semibold text-[18px]'>User Experience Class</div>
                         <div className='mb-2'>Today at 12.00 PM</div>
-                        <button className='bg-[#1DBF73] p-2 rounded-full text-white '>Join Now</button>
+                        <button className='bg-[#1DBF73] py-2 w-[70%] rounded-full text-white mt-2'>Join Now</button>
                     </div>
                 </div>
 
-                <div className='flex absolute bottom-10 right-3  bg-white p-2 space-x-2 items-center font-nu rounded-lg'>
+                <div className='flex absolute bottom-14 right-10 bg-[#FFFFFFCC] p-6 py-5 gap-4 items-center font-nu rounded-xl' style={{ backdropFilter: 'blur(8px)' }}>
                     <div className='flex justify-center items-center h-11 w-11 bg-[#F88C3D] rounded' >
                         <Email />
                     </div>
-                    <div className='flex flex-col '>
+                    <div className='flex flex-col gap-1'>
                         <div className='font-semibold text-[18px]'>Congratulations</div>
                         <div>Your admission completed</div>
                     </div>
