4d845f1f3cfdb94587a19dd3f291d4f739e2eeb0
AnshulKaryal <karyalanshul@gmail.com>
2024-03-26 15:03:41 +0530

    Added Download Brochure

--- src/Components/AiMinds/AiHead.jsx
+++ src/Components/AiMinds/AiHead.jsx
@@ -2,6 +2,26 @@ import './AiMinds.css';
 import Typewriter from 'typewriter-effect';
 
 export default function AiHead(){
+
+    const handleDownloadPdf = () => {
+        // Replace 'path_to_your_pdf.pdf' with the actual path to your PDF file
+        const pdfUrl = 'brochure-26-09.pdf';
+        
+        // Create a hidden anchor element
+        const anchorElement = document.createElement('a');
+        anchorElement.href = pdfUrl;
+        anchorElement.download = 'DownloadedBrochure.pdf'; // Set the filename for downloaded file
+        
+        // Append the anchor element to the body
+        document.body.appendChild(anchorElement);
+        
+        // Trigger the click event on the anchor element to initiate download
+        anchorElement.click();
+        
+        // Remove the anchor element from the body
+        document.body.removeChild(anchorElement);
+      };
+
     return (
         <div className="flex justify-between bg-red-400 px-[5%] py-[4%] Ai-Backimg">
             <div className="w-[60%] flex flex-col justify-center gap-4">
@@ -24,8 +44,8 @@ export default function AiHead(){
                     <p className="font-mons font-medium text-[14px] text-white text-justify leading-7 word-spacing-2">Learn in-demand AI skills such as Deep Learning, Generative AI, NLP & Reinforcement Learning. Master 15+ programming tools & languages along with the latest Gen AI tools while also working on 12+ industry projects.</p>
                 </div>
                 <div className="flex justify-between w-[73%] mt-6">
-                    <button className="font-mons font-bold text-white text-[20px] bg-[#169f63] border-2 border-white rounded-2xl px-14 py-4">Enroll Now</button>
-                    <button className="font-mons font-bold text-white text-[20px] bg-[#169f63] border-2 border-white rounded-2xl px-9 py-4">Download Brochure</button>
+                    {/* <button className="font-mons font-bold text-white text-[20px] bg-[#169f63] border-2 border-white rounded-2xl px-14 py-4">Enroll Now</button> */}
+                    <a onClick={handleDownloadPdf} className="font-mons font-bold text-white text-[20px] bg-[#169f63] border-2 border-white rounded-2xl px-9 py-4 cursor-pointer">Download Brochure</a>
                 </div>
             </div>
             <div className="w-[30%] flex flex-col gap-4 items-end">
