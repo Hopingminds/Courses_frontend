b5f95bda433eee3fe0db2b6da0b2f001cc03559f
LakshitaAgrawal <lakshitagoel888@gmail.com>
2024-04-01 12:56:27 +0530

    mobile

--- src/Components/Courses_Home/index.js
+++ src/Components/Courses_Home/index.js
@@ -1,90 +1,133 @@
-import React, { useEffect, useState } from 'react';
-import './Courses.css';
-import CourseCard from './CourseCard';
-import { COURSESURL } from '../confidential';
-import { BASE_URL } from '../../Api/api';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./Courses.css";
+import CourseCard from "./CourseCard";
+import { COURSESURL } from "../confidential";
+import { BASE_URL } from "../../Api/api";
+import { Link } from "react-router-dom";
 // import { COURSESURL } from '../Confidential';
 
 const Courses_Home = () => {
-    const [selectedCategory, setSelectedCategory] = useState('All Courses');
-    const [selectedCourse, setSelectedCourse] = useState('');
-    const [cardsToShow, setCardsToShow] = useState(4); // State to keep track of number of cards to display
-    const [showAllCards, setShowAllCards] = useState(false);
-    const [courses, setcourses] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState("All Courses");
+  const [selectedCourse, setSelectedCourse] = useState("");
+  const [cardsToShow, setCardsToShow] = useState(4); // State to keep track of number of cards to display
+  const [showAllCards, setShowAllCards] = useState(false);
+  const [courses, setcourses] = useState([]);
 
+  useEffect(() => {
+    async function Fetchdata() {
+      let url = BASE_URL + "/courses";
+      const data = await fetch(url);
+      const response = await data.json();
+      console.log(response);
+      setcourses(response.courses);
+    }
+    Fetchdata();
+  }, []);
 
-    useEffect(() => {
-        async function Fetchdata() {
-            let url = BASE_URL + '/courses'
-            const data = await fetch(url)
-            const response = await data.json();
-            console.log(response);
-            setcourses(response.courses)
+  let filteredCourses =
+    selectedCategory === "All Courses"
+      ? courses
+      : courses.filter((course) => course.category === selectedCategory);
+
+      useEffect(() => {
+        function handleResize() {
+          // Check screen size and set the number of cards to show
+          if (window.innerWidth <= 480) {
+            setCardsToShow(3); // For screens smaller than 480px, show only 4 cards
+          } else {
+            setCardsToShow(4); // For larger screens, show 4 cards
+          }
         }
-        Fetchdata()
-    }, [])
+    
+        // Call handleResize initially and add event listener for resize
+        handleResize();
+        window.addEventListener("resize", handleResize);
+    
+        // Remove event listener on cleanup
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }, []);
 
-    let filteredCourses = selectedCategory === 'All Courses' ? courses : courses.filter(course => course.category === selectedCategory);
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    setSelectedCourse("");
+    // setCardsToShow(4); 
+  };
 
-    const handleCategoryClick = (category) => {
-        setSelectedCategory(category);
-        setSelectedCourse('');
-        setCardsToShow(4); // Reset the number of cards to show when a category is clicked
-    };
+  const handleCourseClick = (courseTitle) => {
+    setSelectedCourse(courseTitle);
+  };
 
-    const handleCourseClick = (courseTitle) => {
-        setSelectedCourse(courseTitle);
-    };
+  const handleShowMoreLessClick = () => {
+    if (showAllCards) {
+      setCardsToShow(4);
+    } else {
+      setCardsToShow(filteredCourses.length);
+    }
+    setShowAllCards(!showAllCards);
+  };
 
+  return (
+    <>
+      {/* <h1 className='text-[40px] mt-16 font-semibold' style={{ textAlign: 'center' }}> Our Feature Courses</h1> */}
 
-    const handleShowMoreLessClick = () => {
-        if (showAllCards) {
-            setCardsToShow(4);
-        } else {
-            setCardsToShow(filteredCourses.length);
-        }
-        setShowAllCards(!showAllCards);
-    };
-
-
-
-    return (
-        <>
-            {/* <h1 className='text-[40px] mt-16 font-semibold' style={{ textAlign: 'center' }}> Our Feature Courses</h1> */}
-
-            <div className="h-[100%] w-full px-20 py-8 mt-12 font-pop bg-[#E2FFF180] space-y-5 xsm:px-2">
-                <div className="flex w-full space-x-10 font-pop xl:space-x-12 xsm:space-x-2 xsm:flex-wrap xsm:gap-y-4 xsm:gap-x-[1px] xsm:justify-center">
-                    {['All Courses', 'Full Stack Development', 'AI & ML', 'Data science', 'Cyber Security', 'Management'].map(category => (
-                        <button key={category} onClick={() => handleCategoryClick(category)} className={selectedCategory === category ? "px-3 py-2 afterclick rounded-full" : "beforeclick"}>
-                            {category}
-                        </button>
-                    ))}
-                </div>
-                <div className='flex justify-between w-full items-center'>
-                <div className="text-4xl font-semibold mt-3">{selectedCategory}</div>
-                <Link to={'/course'} className='text-[#1DBF73] text-[18px] mr-2'>See all</Link>
-                </div>
-                {/* <div className="text-[#333333] text-[20px] mt-1">Our training covers everything you need for a successful career, from basic job skills to advanced tech know-how.</div> */}
-                <div className="my-10  grid grid-cols-4 gap-6 xsm:grid-cols-3 xsm:gap-3 xsm:my-[4%]"> {/* Changed flex to flex-wrap */}
-                    {filteredCourses?.slice(0, cardsToShow)?.map(course => (
-                        <CourseCard
-                            key={course.title}
-                            title={course.title}
-                            price={course.base_price}
-                            firstName={course.instructor.firstName}
-                            lastName={course.instructor.lastName}
-                            duration={course.duration}
-                            image={course.featured_image}
-                            slug={course.slug}
-                            onClick={() => handleCourseClick(course.title)}
-                            isSelected={selectedCourse === course.title}
-                            category={course.category}
-                            description={course.whatWillILearn}
-                        // Pass category to CourseCard component
-                        />
-                    ))}
-                    {/* {filteredCourses.length === 1 && ( // Render additional cards if there is only one course
+      <div className="h-[100%] w-full px-20 py-8 mt-12 font-pop bg-[#E2FFF180] space-y-5 xsm:px-2 xsm:py-4 xsm:mt-0 xsm:space-y-0">
+        <div className="flex w-full space-x-10 font-pop xl:space-x-12 xsm:space-x-2 xsm:flex-wrap xsm:gap-y-2 xsm:gap-x-[1px] xsm:justify-between xsm:text-[8px]">
+          {[
+            "All Courses",
+            "Full Stack Development",
+            "AI & ML",
+            "Data science",
+            "Cyber Security",
+            "Management",
+          ].map((category) => (
+            <button
+              key={category}
+              onClick={() => handleCategoryClick(category)}
+              className={
+                selectedCategory === category
+                  ? "px-3 py-2 afterclick rounded-full xsm:py-1"
+                  : "beforeclick"
+              }
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+        <div className="flex justify-between w-full items-center">
+          <div className="text-4xl font-semibold mt-3 xsm:text-[8px] xsm:mt-0 xsm:ml-2">
+            {selectedCategory}
+          </div>
+          <Link
+            to={"/course"}
+            className="text-[#1DBF73] text-[18px] mr-2 xsm:text-[8px]"
+          >
+            See all
+          </Link>
+        </div>
+        {/* <div className="text-[#333333] text-[20px] mt-1">Our training covers everything you need for a successful career, from basic job skills to advanced tech know-how.</div> */}
+        <div className="my-10  grid grid-cols-4 gap-6 xsm:grid-cols-3 xsm:gap-3 xsm:my-[2%]">
+          {" "}
+          {/* Changed flex to flex-wrap */}
+          {filteredCourses?.slice(0, cardsToShow)?.map((course) => (
+            <CourseCard
+              key={course.title}
+              title={course.title}
+              price={course.base_price}
+              firstName={course.instructor.firstName}
+              lastName={course.instructor.lastName}
+              duration={course.duration}
+              image={course.featured_image}
+              slug={course.slug}
+              onClick={() => handleCourseClick(course.title)}
+              isSelected={selectedCourse === course.title}
+              category={course.category}
+              description={course.whatWillILearn}
+              // Pass category to CourseCard component
+            />
+          ))}
+          {/* {filteredCourses.length === 1 && ( // Render additional cards if there is only one course
                         <>
                             <CourseCard 
                                 title={`${selectedCategory} Course Title 2`} 
@@ -116,18 +159,17 @@ const Courses_Home = () => {
                             />
                         </>
                     )} */}
-                </div>
-                {/* {filteredCourses?.length > 4 && ( // Render 'Show more' button if there are more than 4 courses
+        </div>
+        {/* {filteredCourses?.length > 4 && ( // Render 'Show more' button if there are more than 4 courses
                     <div className="flex justify-center mt-5" style={{float:'right'}}>
                     <button className="px-4 py-2 bg-blue-500 text-white rounded-md" onClick={handleShowMoreLessClick}>
                         {showAllCards ? 'Show Less' : 'Show More'}
                     </button>
                 </div>
                 )} */}
-            </div>
-
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default Courses_Home;
--- src/Components/MyLearning/WishList.js
+++ src/Components/MyLearning/WishList.js
@@ -28,33 +28,33 @@ export default function WishList(){
   
       }, [])
     return (
-        <div className="my-6 mx-[5%] grid grid-cols-4 gap-y-6 gap-x-[20px]">
+        <div className="my-6 mx-[5%] grid grid-cols-4 gap-y-6 gap-x-[20px] xsm:mt-3 xsm:gap-3 xsm:grid-cols-3">
             {
                 Data?.map((item)=>{
                     return(<>
-                    <Link to={`/detailcourse/${item?.course?.slug}`} className=" px-4 py-6 mt-2 h-[350px] rounded-xl shadow-xl shadow-[#D9D9D9]">
-                <div className="h-[55%]">
-                    <img className="w-full h-full" src={item.course.featured_image}/>
+                    <Link to={`/detailcourse/${item?.course?.slug}`} className=" px-4 py-6 mt-2 h-[350px] rounded-xl shadow-xl shadow-[#D9D9D9] xsm:mt-0 xsm:rounded-md xsm:p-2 xsm:h-full">
+                <div className="h-[55%] xsm:h-[45%]">
+                    <img className="w-full h-full rounded-lg" src={item.course.featured_image}/>
                 </div>
-                <div className="space-y-2 mt-2 ">
-                    <div className="flex justify-between mt-6">
-                        <div className="flex space-x-2 items-center">
-                            <img className="w-[16px] h-[16px]" src="../Icons/RCDesign.svg"/>
-                            <p className="font-pop text-[12px] font-medium text-[#555555]">{item.course.category}</p>
+                <div className="space-y-2 mt-2 xsm:mt-0">
+                    <div className="flex justify-between mt-6 xsm:mt-2">
+                        <div className="flex space-x-2 items-center xsm:space-x-0">
+                            <img className="w-[16px] h-[16px] xsm:w-2 xsm:h-2" src="../Icons/RCDesign.svg"/>
+                            <p className="font-pop text-[12px] font-medium text-[#555555] xsm:text-[5px]">{item.course.category}</p>
                         </div>
-                        <div className="flex space-x-2 items-center">
-                            <img className="w-[16px] h-[16px] text-[#555555]" src="../Icons/RCClock.svg"/>
-                            <p className="font-pop text-[12px] font-medium text-[#555555]">{item.course.duration}</p>
+                        <div className="flex space-x-2 items-center xsm:space-x-0">
+                            <img className="w-[16px] h-[16px] text-[#555555] xsm:w-2 xsm:h-2" src="../Icons/RCClock.svg"/>
+                            <p className="font-pop text-[12px] font-medium text-[#555555] xsm:text-[5px]">{item.course.duration}</p>
                         </div>
                     </div>
-                    <p className="font-pop font-semibold text-[18px] text-[#252641]">{item.course.title}</p>
-                    <div className=" flex items-center justify-between">
+                    <p className="font-pop font-semibold text-[18px] text-[#252641] xsm:text-[8px]">{item.course.title}</p>
+                    <div className=" flex flex-row items-center justify-between">
                         <div className="flex items-center space-x-3">
-                            <img className="w-[32px] h-[32px]" src="../img/RCimg2.png"/>
-                            <p className="font-pop font-medium text-[14px]">{item.course.instructor.firstName} {item.course.instructor.lastName}</p>
+                            <img className="w-[32px] h-[32px] xsm:w-5 xsm:h-5" src="../img/RCimg2.png"/>
+                            <p className="font-pop font-medium text-[14px] xsm:text-[5px]">{item.course.instructor.firstName} {item.course.instructor.lastName}</p>
                         </div>
                         <div>
-                            <p className="font-pop font-bold text-[#49BBBD] text-[18px]">₹{item.course.base_price}</p>
+                            <p className="font-pop font-bold text-[#49BBBD] text-[18px] xsm:text-[5px]">₹{item.course.base_price}</p>
                         </div>
                     </div>
                     
