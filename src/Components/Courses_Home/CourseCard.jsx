865fe440ba752643fc56457c802b381d4ba2e519
LakshitaAgrawal <lakshitagoel888@gmail.com>
2024-04-03 17:21:47 +0530

    home page cards

--- src/Components/Courses_Home/CourseCard.jsx
+++ src/Components/Courses_Home/CourseCard.jsx
@@ -34,7 +34,7 @@ const CourseCard = ({
       onMouseEnter={() => toggleHover(ind)}
       onMouseLeave={() => toggleHover(null)}
       href={`/detailcourse/${slug}`}
-      className={`shadow-lg  max-w-sm font-pop rounded-3xl relative coursecardhome   bg-white p-2 xsm:rounded-lg  ${
+      className={`shadow-lg  max-w-sm font-pop rounded-2xl relative coursecardhome   bg-white p-2 xsm:rounded-lg w-[283px] h-[443px] ${
         isSelected ? "border-2 border-blue-500" : ""
       }`}
     >
@@ -49,17 +49,17 @@ const CourseCard = ({
         muted
       />:<img
       // style={{ height: "10rem" }}
-      className="w-full  rounded-2xl h-[40%] xsm:h-[45%] md:h-[35%]"
+      className="w-full  rounded-t-xl h-[45%] xsm:h-[45%] md:h-[35%]"
       src={image}
       alt="Course"
     />
       }
       
-      <div className=" h-[55%] xsm:h-[45%]">
+      <div className=" h-[45%] xsm:h-[45%]">
         <div className="px-2 flex justify-between items-center h-[15%] text-[#474747] mt-4">
           <div className="flex items-center gap-2">
-              <img src="/lina.png" className="h-10 w-10 bg-[#0000001A] rounded-full xsm:h-4 xsm:w-4 md:h-6 md:w-6" />
-              <div className="font-medium text-[16px] xsm:text-[6px] md:text-[8px]">{firstName} {lastName}</div>
+              <img src="/lina.png" className="h-8 w-8 bg-[#0000001A] rounded-full xsm:h-4 xsm:w-4 md:h-6 md:w-6" />
+              <div className="font-medium font-pop text-[16px] xsm:text-[6px] md:text-[8px]">{firstName} {lastName}</div>
             </div>
           {/* <div className="flex space-x-2 items-center xsm:space-x-1">
             <Design className="h-4 w-4 xsm:h-2 xsm:w-2 md:w-3 md:h-3"/>
@@ -77,11 +77,11 @@ const CourseCard = ({
             <Clock className="h-4 w-4 xsm:h-2 xsm:w-2 md:w-3 md:h-3"/>
             <p className="text-[12px] xsm:text-[6px] text-[#696984] md:text-[8px]"> {duration}</p>
           </div> */}
-        <div className="px-2 pt-4 space-y-2 flex  flex-col justify-between h-[85%] xsm:mt-1 xsm:py-1 xsm:px-0 xsm:justify-between xsm:space-y-0 md:py-2 md:mt-0">
+        <div className="px-2 pt-4 flex gap-6 flex-col justify-between h-[85%] xsm:mt-1 xsm:py-1 xsm:px-0 xsm:justify-between xsm:space-y-0 md:py-2 md:mt-0">
           <div>
             <div
               style={{ color: "#3C3C3C" }}
-              className="font-bold text-[16px] mb-2 h-12 xsm:text-[8px] xsm:mb-0 xsm:h-4 md:text-[10px] md:mb-0"
+              className="font-semibold text-[16.5px] mb-2 h-12 xsm:text-[8px] xsm:mb-0 xsm:h-4 md:text-[10px] md:mb-0"
             >
               {cropString(title, 50)}
             </div>
@@ -91,12 +91,12 @@ const CourseCard = ({
           </div>
           <div className="flex justify-between items-center ">
             <div className="flex space-x-2 items-center xsm:space-x-1">
-              <Design className="h-4 w-4 xsm:h-2 xsm:w-2 md:w-3 md:h-3"/>
-              <span className="text-[12px] xsm:text-[6px] md:text-[8px]">{category}</span>
+              <Design className="h-4 w-4 text-[#808080] xsm:h-2 xsm:w-2 md:w-3 md:h-3"/>
+              <span className="text-[14px] xsm:text-[6px] text-[#808080] md:text-[8px]">{category}</span>
             </div>
-            <div className=" flex items-center mt-2 space-x-2 xsm:space-x-1 md:mt-1">
-              <Clock className="h-4 w-4 xsm:h-2 xsm:w-2 md:w-3 md:h-3"/>
-              <p className="text-[12px] xsm:text-[6px] text-[#696984] md:text-[8px]"> {duration}</p>
+            <div className=" flex items-center space-x-2 xsm:space-x-1 md:mt-1">
+              <Clock className="h-4 w-4 text-[#808080] xsm:h-2 xsm:w-2 md:w-3 md:h-3"/>
+              <p className="text-[14px] xsm:text-[6px] text-[#808080] md:text-[8px]"> {duration}</p>
             </div>
             {/* <div className="flex items-center space-x-1">
               <img src="/lina.png" className="h-10 w-10 xsm:h-4 xsm:w-4 md:h-6 md:w-6" />
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
                 <div className='flex  justify-between px-[5%] xsm:pl-[5%] xsm:flex xsm:flex-col xsm:gap-8'>
                     <div className='flex flex-col gap-5 w-[260px] xsm:gap-2 xsm:w-[90%] md:gap-6'>
                         <div onClick={() => navigate('/')}><img src='/logo.png' className='w-auto h-[50px] xsm:w-[40%] xsm:h-[10%] md:w-[60%]  cursor-pointer' /></div>
-                        <p className='text-white text-[16px] font-pop text-justify w-[90%] xsm:text-[10px] md:text-[12px] md:w-[90%] md:pl-2'>HopingMinds offers holistic development programs designed to position students in high-growth roles through our network of over 150+ corporate partners. Our tailored approach ensures you're not just prepared, but primed for success in your chosen field.</p>
+                        <p className='text-white text-[16px] font-pop text-balance w-[90%] xsm:text-[10px] md:text-[12px] md:w-[90%] md:pl-2'>HopingMinds offers holistic development programs designed to position students in high-growth roles through our network of over 150+ corporate partners. Our tailored approach ensures you're not just prepared, but primed for success in your chosen field.</p>
                     </div>
                     <div className='flex flex-col gap-8 xsm:gap-2  '>
                         <p className='text-white text-[20px] font-pop font-semibold xsm:text-[10px] md:text-[14px]'>GET HELP</p>
