1f9dcd541e2947d97f26d3e4722e82736fa57021
Davinder9297 <davindergiri765@gmail.com>
2024-04-05 17:16:58 +0530

    updated

--- src/Components/SubAdminDashboard/DashboardSubAdmin.js
+++ src/Components/SubAdminDashboard/DashboardSubAdmin.js
@@ -1,17 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import BannarSubAdmin from "./BannarSubAdmin";
 import FilterSubAdmin from "./FilterSubAdmin";
 import DataDashboard from "./DataDashboard";
 import DetailTableDashboard from "./DetailTableDashboard";
 import NavSubAdmin from "./NavSubAdmin";
 import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../Api/api";
 
 const DashboardSubAdmin = () => {
+  const [data, setdata] = useState()
   let navigate=useNavigate()
 useEffect(() => {
   if(!localStorage.getItem('token')){
     navigate('/subadmin-login')
       }
+}, [])
+useEffect(() => {
+  async function FetchData(){
+    try {
+      let url=BASE_URL+'/getadmindashdata?university=cu'
+      const data=await fetch(url);
+      const response=await data.json()
+      setdata(response?.data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  FetchData()
 }, [])
 
   return (
@@ -21,9 +36,9 @@ useEffect(() => {
       <div className="flex flex-col gap-14 pb-20 bg-[#FCF8F8]">
         <div className="flex flex-row">
           <FilterSubAdmin />
-          <DataDashboard />
+          <DataDashboard data={data}/>
         </div>
-        <DetailTableDashboard />
+        <DetailTableDashboard data={data?.users}/>
       </div>
     </>
   );
