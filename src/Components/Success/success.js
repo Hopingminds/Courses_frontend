6af21c271dc369971baa51ab405743d98240fc8b
LakshitaAgrawal <lakshitagoel888@gmail.com>
2024-04-08 16:06:43 +0530

    responsive

--- src/Components/Success/success.js
+++ src/Components/Success/success.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Success() {
     return (
-        <div className='thankpage my-5 xsm:mx-2'>
+        <div className='thankpage my-5 xsm:mx-2 md:flex md:justify-center md:items-center xsm:flex xsm:justify-center'>
             <div className="thank-you">
                 <img width="188" height="188" src="/ok.gif" alt="ok" />
                 <h1 className='text-[#1DBF73] font-semibold font-pop'>Payment Successful</h1>
