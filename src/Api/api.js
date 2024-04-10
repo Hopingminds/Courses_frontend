67dd5a02b587559c31ff2b6416367ca3c8887a85
vivek kumar <vivekr4400@gmail.com>
2024-04-10 11:07:00 +0530

    fixed issue

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://api.hopingminds.in/api';
-//export const BASE_URL = 'https://courses-api.up.railway.app/api';
+// export const BASE_URL = 'http://api.hopingminds.in/api';
+export const BASE_URL = 'https://courses-api.up.railway.app/api';
 // export const BASE_URL = 'http://localhost:3009/api';
