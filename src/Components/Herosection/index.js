57b630d7d7cc9fe9fb1517cb23707f59d38580f8
LakshitaAgrawal <lakshitagoel888@gmail.com>
2024-04-17 09:36:56 +0530

    updated

--- src/Components/Herosection/index.js
+++ src/Components/Herosection/index.js
@@ -13,8 +13,8 @@ import ReactPlayer from 'react-player'
 //                 {/* <div className='text-[3.2vw] font-semibold leading-[4.0rem] 2xl:leading-[5.5rem]'>Shape Your Tomorrow, Today with HopingMinds: Where Education Meets Aspiration</div> */}
 //                 <div className='text-[40px] font-semibold leading-[3rem] 2xl:leading-[5.5rem] font-pop xsm:text-[15px] xsm:leading-5 md:text-[24px] md:leading-normal'>Master In-Demand Skills for Dream Placements
 // High Impact Courses that Earn you College Credits</div>
-//                 {/* <div className='font-nu text-[18px] mt-5 w-[83%] text-justify xsm:text-[10px] xsm:text-start'>"HopingMinds offers holistic development programs designed to position students in high-growth roles through our network of over 150 corporate partners. Our tailored approach ensures you're not just prepared, but primed for success in your chosen field."</div> */}
-//                 {/* <div className='font-nu text-[18px] mt-5 w-[83%] text-justify xsm:text-[10px] xsm:text-start md:text-[12px]'>"HopingMinds offers holistic development programs designed to position students in high-growth roles through our network of over 150 corporate partners. Our tailored approach ensures you're not just prepared, but primed for success in your chosen field."</div> */}
+//                 {/* <div className='font-nu text-[18px] mt-5 w-[83%] text-justify xsm:text-[6px] xsm:text-start'>"HopingMinds offers holistic development programs designed to position students in high-growth roles through our network of over 150 corporate partners. Our tailored approach ensures you're not just prepared, but primed for success in your chosen field."</div> */}
+//                 {/* <div className='font-nu text-[18px] mt-5 w-[83%] text-justify xsm:text-[6px] xsm:text-start md:text-[12px]'>"HopingMinds offers holistic development programs designed to position students in high-growth roles through our network of over 150 corporate partners. Our tailored approach ensures you're not just prepared, but primed for success in your chosen field."</div> */}
 
 //             </div>
 //             <div className='w-[55%] flex justify-center items-center  relative text-[#545567] mb-5 pb-5 md:mb-0 md:pb-0 md:justify-end xsm:mb-0 xsm:pb-0'>
@@ -53,36 +53,36 @@ useEffect(() => {
 }, [active])
 
     return (<>
-        <div className=" bg-gradient-to-r from-[#0F2027] to-[#203A43] h-[80vh] hero w-full flex font-pop justify-center items-center px-[3%]">
+        <div className=" bg-gradient-to-r from-[#0F2027] to-[#203A43] h-[80vh] hero w-full flex font-pop justify-center items-center px-[3%] xsm:h-[50vh]">
             <div className='w-[45%] space-y-8'>
-                <div className='text-[45px] font-semibold text-white leading-[60px] font-pop text-wrap'>Master In-Demand Skills for Dream Placements</div>
-                <div className='text-[28px] font-medium text-white leading-[40px] font-pop text-wrap'>High Impact Courses that Earn you College Credits</div>
+                <div className='text-[45px] font-semibold text-white leading-[60px] font-pop text-wrap xsm:text-[20px] xsm:leading-[28px]'>Master In-Demand Skills for Dream Placements</div>
+                <div className='text-[28px] font-medium text-white leading-[40px] font-pop text-wrap xsm:text-[14px] xsm:leading-[18px]'>High Impact Courses that Earn you College Credits</div>
             </div>
             <div className='w-[40%] h-[75%] flex justify-center items-center  relative text-[#545567] '>
-                <div className='h-[80%] w-[100%] herobox bg-white  flex items-center '>
+                <div className='h-[80%] w-[100%] herobox bg-white  flex items-center xsm:h-[50%]'>
                     {/* <ReactPlayer className='-translate-x-28' width='150%' height='100%'  url='/home_video.mp4'  playing={true}  loop={true} controls={false} /> */}
                 <img  className=' h-full w-full'  src="/homegif.gif" alt="video" />
 
                 </div>
-                <div className={`${active==1?'scale-in-center':''} flex justify-center w-[15vw] h-[16vh] backdrop-blur-[2px] absolute -top-6 left-10 px-2 space-x-2 items-center  rounded-lg bgcurvehome`}>
-                    <Certificate className='w-16 h-14'/>
+                <div className={`${active==1?'scale-in-center':''} flex justify-center w-[15vw] h-[16vh] backdrop-blur-[2px] absolute -top-6 left-10 px-2 space-x-2 items-center  rounded-lg bgcurvehome xsm:space-x-0 xsm:w-[20vw] xsm:h-[7vh] xsm:top-4 xsm:left-6`}>
+                    <Certificate className='w-16 h-14 xsm:w-6 xsm:h-4'/>
                     <div className='flex flex-col text-white w-[60%] '>
-                        <div>NSDC Partner with Course Certifications</div>
+                        <div className='xsm:text-[5px]'>NSDC Partner with Course Certifications</div>
                     </div>
                 </div>
-                <div className={`${active==2?'scale-in-center':''} flex justify-center absolute w-[12vw] h-[14vh] backdrop-blur-[2px]  top-[70%] -left-10 px-2  space-x-2 items-center bgcurvehome`}>
-                    <Ai className='w-12 h-12'/>
+                <div className={`${active==2?'scale-in-center':''} flex justify-center absolute w-[12vw] h-[14vh] backdrop-blur-[2px]  top-[70%] -left-10 px-2  space-x-2 items-center bgcurvehome xsm:space-x-1 xsm:w-[22vw] xsm:h-[7vh] xsm:-left-1`}>
+                    <Ai className='w-12 h-12 xsm:w-5 xsm:h-5'/>
                     <div className='flex flex-col text-white  w-[60%] '>
-                        <div className=''>AI driven Learning Experience</div>
+                        <div className='xsm:text-[5px]'>AI driven Learning Experience</div>
                     </div>
                 </div>
 
-                <div className={` ${active==3?'scale-in-center':''} flex justify-center absolute bottom-10 -right-5 w-[15vw] h-[16vh] backdrop-blur-[2px] px-2 space-x-2 items-center   bgcurvehome`}>
+                <div className={` ${active==3?'scale-in-center':''} flex justify-center absolute bottom-10 -right-5 w-[15vw] h-[16vh] backdrop-blur-[2px] px-2 space-x-2 items-center   bgcurvehome xsm:px-0 xsm:pr-2 xsm:space-x-0 xsm:w-[20vw] xsm:h-[7vh]`}>
                     <div className='flex justify-center items-center h-11 w-11  rounded' >
-                        <Cashback className='w-16 h-14'/>
+                        <Cashback className='w-16 h-14 xsm:w-6 xsm:h-4'/>
                     </div>
-                    <div className='flex flex-col text-white  w-[60%] '>
-                        <div>Earn College Credits for Each Course</div>
+                    <div className='flex flex-col text-white  w-[60%] xsm:w-[70%]'>
+                        <div className='xsm:text-[5px]'>Earn College Credits for Each Course</div>
                     </div>
                 </div>
             </div>
