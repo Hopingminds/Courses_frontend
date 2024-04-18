4dfe5fcf65d2ad0964ca12be8c776ae02f08f2bf
LakshitaAgrawal <lakshitagoel888@gmail.com>
2024-04-18 14:28:29 +0530

    updated

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -7,15 +7,14 @@ import Herosection from "../Herosection";
 import OurFeatures from "../OurFeatures/OurFeatures";
 import Testimonial from "../Testimonial/Testimonial";
 import WhatHM from "../WhatHM/WhatHM";
-import logo from '../../Assests/Images/hmlogo.png'
+import logo from "../../Assests/Images/hmlogo.png";
 import PapHome from "./paphome";
 import NewTestimonial from "../Testimonial/NewTestimonial";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-
-    const [showPopup, setShowPopup] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -35,37 +34,39 @@ export default function Home() {
     };
   }, []);
 
+  return (
+    <>
+      <head>
+        <title>Hoping Minds</title>
+        <meta charset="UTF-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="HopingMinds" />
+        <meta property="og:image" content="/src/Assests/Images/hmlogo.png" />
+        <meta name="twitter:card" content="/src/Assests/Images/hmlogo.png" />
+        <meta
+          name="description"
+          content="Holistic development programs that place students in specific high growth roles across 150+ Corporate Partners."
+        />
+      </head>
+      <div className="space-y-8 xsm:space-y-6 md:space-y-10">
+        <Herosection />
+        <Companies />
+        <Courses_Home />
+        <WhatHM />
+        <OurFeatures />
+        <PapHome />
+        <Certificate />
+        <Testimonial />
+      </div>
 
-    return (<>
-        <head>
-            <title>Hoping Minds</title>
-            <meta charset="UTF-8" />
-            <link rel="icon" href="/favicon.ico" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta property="og:title" content="HopingMinds" />
-            <meta property="og:image" content="/src/Assests/Images/hmlogo.png" />
-            <meta name="twitter:card" content="/src/Assests/Images/hmlogo.png" />
-            <meta name="description" content="Holistic development programs that place students in specific high growth roles across 150+ Corporate Partners." />
-
-        </head>
-        <div className="space-y-8 xsm:space-y-6 md:space-y-10">
-            <Herosection />
-            <Companies />
-            <Courses_Home/>
-            <WhatHM />
-            <OurFeatures />
-            <PapHome />
-            <Certificate />
-            <Testimonial />
-        </div>
-
-        {showPopup && (
-        <div className="fixed bottom-16 right-0 bg-white p-4 shadow-lg">
-            <Link to={"/resumebuilder"}>
-          <button>CV Builder</button>
-          </Link>
-        </div>
+      {showPopup && (
+        <Link to={"/resumebuilder"}>
+          <div className="fixed bottom-10 right-0 bg-gradient-to-r from-[#0F2027] via-[#0B1418] to-[#203A43] text-white py-4 px-6 drop-shadow-xl rounded-l-md ">
+            <button className="italic">Build Your CV</button>
+          </div>
+        </Link>
       )}
-
-    </>)
+    </>
+  );
 }
--- src/Components/ResumeBuilder/ResumeBuilder.jsx
+++ src/Components/ResumeBuilder/ResumeBuilder.jsx
@@ -21,7 +21,7 @@ const ResumeBuilder = () => {
     return (
         <div className={`w-[60vw] bg-white rounded-xl my-10 xsm:w-[70vw] ${location.pathname === '/resumebuilder' ?"mx-auto":""}`}>
             <div className='grid grid-cols-4'>
-                <button className={`border border-[#EAEAEA] rounded-t-2xl rounded-r-none py-2 font-pop font-semibold text-[18px] md:text-[16px] xsm:text-[12px] ${activeComponent === 'basic' ? 'resumebutton' : ''}`} onClick={() => renderComponent('basic')}>Basic</button>
+                <button className={`border border-[#EAEAEA] rounded-tl-2xl rounded-r-none py-2 font-pop font-semibold text-[18px] md:text-[16px] xsm:text-[12px] ${activeComponent === 'basic' ? 'resumebutton' : ''}`} onClick={() => renderComponent('basic')}>Basic</button>
                 <button className={`border border-[#EAEAEA] py-2 font-pop font-semibold text-[18px] md:text-[16px] xsm:text-[12px] ${activeComponent === 'education' ? 'resumebutton' : ''}`} onClick={() => renderComponent('education')}>Education</button>
                 <button className={`border border-[#EAEAEA] py-2 font-pop font-semibold text-[18px] md:text-[16px] xsm:text-[12px] ${activeComponent === 'technical' ? 'resumebutton' : ''}`} onClick={() => renderComponent('technical')}>Technical</button>
                 <button className={`border border-[#EAEAEA] rounded-r-2xl rounded-br-none py-2 font-pop font-semibold text-[18px] md:text-[16px] xsm:text-[12px] ${activeComponent === 'otherinfo' ? 'resumebutton' : ''}`} onClick={() => renderComponent('otherinfo')}>Other Info</button>
