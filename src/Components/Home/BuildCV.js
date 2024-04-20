1f59c2933be976a694bbefedf4d207296a586bfd
LakshitaAgrawal <lakshitagoel888@gmail.com>
2024-04-20 12:54:44 +0530

    cv builder section

---
+++ src/Components/Home/BuildCV.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./BuildCV.css";
+import BuilderBg from "../../Assests/Images/builcv-bg.png";
+
+const BuildCV = () => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${BuilderBg})` }}
+      className="w-full flex flex-col gap-16 justify-center items-center text-white font-pop font-semibold py-[4%] mt-20 md:gap-12 xsm:gap-6 xsm:py-[5%]"
+    >
+      <div className="flex flex-col gap-8 justify-center items-center w-[70%] md:gap-6 xsm:w-[80%] xsm:gap-4">
+        <p className="text-[36px] text-center md:text-[30px] md:leading-tight xsm:text-[14px] xsm:leading-tighter">
+          Create your professional Resume with our designs
+        </p>
+        <p className="text-[20px] text-center md:text-[14px] md:leading-tight xsm:text-[8px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <Link to={"/resumebuilder"} className="flex justify-center items-center">
+        <button className="button-resume font-pop">
+              <span>Build Your Resume</span>
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default BuildCV;
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -12,6 +12,7 @@ import PapHome from "./paphome";
 import NewTestimonial from "../Testimonial/NewTestimonial";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import BuildCV from "./BuildCV";
 
 export default function Home() {
   const [showPopup, setShowPopup] = useState(false);
@@ -57,14 +58,15 @@ export default function Home() {
         <OurFeatures />
         <PapHome />
         <Certificate />
+        <BuildCV />
         <Testimonial />
       </div>
 
       {showPopup && (
         <Link to={"/resumebuilder"}>
-          <div className="fixed bottom-10 right-0 bg-gradient-to-r from-[#0F2027] via-[#0B1418] to-[#203A43] text-white py-4 px-6 drop-shadow-xl rounded-l-md ">
-            <button className="italic">
-              <span className="">Build Your CV</span>
+          <div className="fixed bottom-10 right-0 bg-gradient-to-r from-[#0F2027] via-[#0B1418] to-[#203A43] text-white py-4 px-6 drop-shadow-xl rounded-l-md xsm:py-2 xsm:px-4 xsm:bottom-6 md:bottom-8 md:py-3 md:px-5">
+            <button className="italic flex justify-center items-center">
+              <span className="xsm:text-[10px] md:text-[14px]">Build Your CV</span>
             </button>
           </div>
         </Link>
