c333be11717d7e1579e605b62aaae3873d18abd0
Anuraag Kumar Mishra <65799896+aadis21@users.noreply.github.com>
2024-04-23 20:16:56 +0530

    Update WhyHM.jsx

--- src/Components/HireFromUs/WhyHM.jsx
+++ src/Components/HireFromUs/WhyHM.jsx
@@ -67,7 +67,7 @@ const WhyHM = () => {
             <div className="text-center text-[#1DBF73] font-semibold text-2xl md:text-[16px] xsm:text-[14px] xsm:leading-tight">Immediate Joining</div>
             <div className="flex gap-x-2 ">
               <Correct className="h-5 w-5 md:h-4 md:w-4 xsm:h-3 xsm:w-3"/>
-              <div className="hmboxline text-sm md:text-[12px] xsm:text-[10px] xsm:leading-tight">We have pretrained developers.</div>
+              <div className="hmboxline text-sm md:text-[12px] xsm:text-[10px] xsm:leading-tight">Rapid Skill Acquisition.</div>
             </div>
             <div className="flex gap-x-2">
               <Correct className="h-5 w-5 md:h-4 md:w-4 xsm:h-3 xsm:w-3"/>
