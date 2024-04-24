7a0bf7dcdccec9455a0e0f8141571a9d4e85058b
vivek kumar <vivekr4400@gmail.com>
2024-04-24 15:39:18 +0530

    updated changes

--- src/Components/Herosection/index.js
+++ src/Components/Herosection/index.js
@@ -144,16 +144,16 @@ export default function Herosection() {
 
   useEffect(() => {
     if (currentIndex < remainingText.length) {
-        const interval = setInterval(() => {
-            setDisplayText((prevText) => prevText + remainingText[currentIndex]);
-            setCurrentIndex((prevIndex) => prevIndex + 1);
-        }, 200);
-        return () => clearInterval(interval);
+      const interval = setInterval(() => {
+        setDisplayText((prevText) => prevText + remainingText[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, 200);
+      return () => clearInterval(interval);
     } else {
-        setDisplayText(staticText);
-        setCurrentIndex(0);
+      setDisplayText(staticText);
+      setCurrentIndex(0);
     }
-}, [currentIndex, remainingText.length, staticText]);
+  }, [currentIndex, remainingText.length, staticText]);
 
 
   return (
@@ -175,11 +175,7 @@ export default function Herosection() {
             {" "}
             Unlock Opportunities with 200+ Leading Corporates, Master Skills with Industry Experts, and Stay up-skilled with our Annually Updated Curriculum; Experience Personalized Career Coaching and Exclusive Networking Events!
           </div>
-<<<<<<< HEAD
-          <div className="text-[25px] text-[#1DBF73] h-16 w-[75%] font-pop text-wrap md:text-[18px] xsm:text-[12px] xsm:h-8">
-=======
           <div className="text-[25px] text-[#1DBF73] h-16 w-[75%] font-pop text-wrap md:text-[18px] xsm:text-[10px] xsm:h-8">
->>>>>>> cabc3556b7360e0ef94014c70894a33303e09ae3
             <span className="text-white">High Impact</span>
             {displayText}
           </div>
