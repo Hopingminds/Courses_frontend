ba131a9a39d2429a67f19a0484c0578d95ced2b3
Davinder9297 <davindergiri765@gmail.com>
2024-04-27 16:11:19 +0530

    updated

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,10 +2,6 @@
 // export const BASE_URL = 'http://localhost:8080/api';
 // export const AUTH_BASE_URL = 'http://localhost:8080/auth';
 
-/** DEV */
-export const BASE_URL = 'http://192.168.0.164:8000/api';
-export const AUTH_BASE_URL = 'http://192.168.0.164:8000/auth';
-
 /** PROD */
-// export const BASE_URL = 'https://api.hopingminds.in/api';
-// export const AUTH_BASE_URL = 'https://api.hopingminds.in/auth';
+export const BASE_URL = 'https://api.hopingminds.in/api';
+export const AUTH_BASE_URL = 'https://api.hopingminds.in/auth';
