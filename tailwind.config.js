2ae65d8272ca94cb9b0d2cda89a98b60018b8a72
LakshitaAgrawal <lakshitagoel888@gmail.com>
2024-05-25 11:19:18 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,8 +25,9 @@ module.exports = {
     },
     screens: {
       'xsm': { 'min': '320px', 'max': '480px' },
+      'sm': { 'min': '481px', 'max': '720px'},
       'md': { 'min': '721px', 'max': '1024px' },
-      'lg': { 'min': '1025px', 'max': '1279px' },
+      'lg': { 'min': '1025px', 'max': '1599px' },
       'xl': { 'min': '1600px', 'max': '1900px' },
       '2xl': { 'min': '1910px', 'max': '2000px'},
     },
