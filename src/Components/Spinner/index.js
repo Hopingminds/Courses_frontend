4402a5be1bb6f8854a8fa0df0aa29c5fbec8b6b2
vivek kumar <vivekr4400@gmail.com>
2024-06-22 12:04:49 +0530

    updated

--- src/Components/Spinner/index.js
+++ src/Components/Spinner/index.js
@@ -1,7 +1,7 @@
 import './spinner.css'
 export default function Spinner(){
     return(<>
-    <div className='h-full w-full flex justify-center items-center bg-transparent'>
+    <div className='h-full w-full flex justify-center items-center bg-transparent z-[99999999788]'>
     <span className="loader "></span>
     </div>
     </>)
