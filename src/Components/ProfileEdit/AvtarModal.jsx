84a386ed27694de117fe41e710b62518f8db86d9
Avinash kumar pandey <105911954+avinash-kr-pandey@users.noreply.github.com>
2024-07-05 11:25:52 +0530

    responsive profile

--- src/Components/ProfileEdit/AvtarModal.jsx
+++ src/Components/ProfileEdit/AvtarModal.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { BASE_URL } from "../../Api/api";
 import { toast } from "react-toastify";
 
-const AvtarModal = ({ onClose, updateProfilePicture }) => {
+const AvatarModal = ({ onClose, updateProfilePicture }) => {
   const avatars = [
     { id: 1, name: "Avatar 1", image: "./Avtars/Avtar-1.png" },
     { id: 2, name: "Avatar 2", image: "./Avtars/Avtar-2.png" },
@@ -16,6 +16,12 @@ const AvtarModal = ({ onClose, updateProfilePicture }) => {
     { id: 8, name: "Avatar 8", image: "./Avtars/Avtar-8.png" },
     { id: 9, name: "Avatar 9", image: "./Avtars/Avtar-9.png" },
     { id: 10, name: "Avatar 10", image: "./Avtars/Avtar-10.png" },
+    { id: 11, name: "Avatar 5", image: "./Avtars/Avtar-5.png" },
+    { id: 12, name: "Avatar 6", image: "./Avtars/Avtar-6.png" },
+    { id: 13, name: "Avatar 7", image: "./Avtars/Avtar-7.png" },
+    { id: 14, name: "Avatar 8", image: "./Avtars/Avtar-8.png" },
+    { id: 15, name: "Avatar 9", image: "./Avtars/Avtar-9.png" },
+    { id: 16, name: "Avatar 10", image: "./Avtars/Avtar-10.png" },
   ];
 
   const [btnLoader, setBtnLoader] = useState(false);
@@ -23,6 +29,9 @@ const AvtarModal = ({ onClose, updateProfilePicture }) => {
   const fileInputRef = useRef(null);
 
   useEffect(() => {
+    // Prevent background scrolling when the modal is open
+    document.body.style.overflow = 'hidden';
+
     const handleClickOutside = (event) => {
       if (modalRef.current && !modalRef.current.contains(event.target)) {
         onClose();
@@ -33,6 +42,7 @@ const AvtarModal = ({ onClose, updateProfilePicture }) => {
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
+      document.body.style.overflow = 'auto'; // Restore background scrolling when the modal is closed
     };
   }, [onClose]);
 
@@ -65,14 +75,13 @@ const AvtarModal = ({ onClose, updateProfilePicture }) => {
   };
 
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-      <div className="bg-white p-4 rounded-lg w-full max-w-5xl" ref={modalRef}>
+    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 p-4 mt-[3rem]">
+      <div className="bg-white p-4 rounded-lg w-full max-w-5xl h-full overflow-y-auto" ref={modalRef}>
         <div className="flex justify-end">
           <RxCross1 className="w-4 h-4 cursor-pointer" onClick={onClose} />
         </div>
         <div className="p-4">
-          {/* <p>Avatar Modal Content</p> */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+          <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
             {avatars.map((avatar) => (
               <div
                 key={avatar.id}
@@ -82,7 +91,7 @@ const AvtarModal = ({ onClose, updateProfilePicture }) => {
                 <img
                   src={avatar.image}
                   alt={avatar.name}
-                  className="xsm:w-[21rem] w-40 h-40 object-cover rounded-lg"
+                  className="w-40 h-35 object-fit rounded-lg "
                 />
                 <p className="text-center">{avatar.name}</p>
               </div>
@@ -94,4 +103,4 @@ const AvtarModal = ({ onClose, updateProfilePicture }) => {
   );
 };
 
-export default AvtarModal;
+export default AvatarModal;
