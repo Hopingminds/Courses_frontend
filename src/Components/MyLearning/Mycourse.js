f459d818fdc809aaee5d283edf8fdc08f5af40af
Davinder9297 <davindergiri765@gmail.com>
2024-07-19 18:37:00 +0530

    updated

--- src/Components/MyLearning/Mycourse.js
+++ src/Components/MyLearning/Mycourse.js
@@ -4,7 +4,29 @@ import { Link } from "react-router-dom";
 
 export default function Mycourse({ courses }) {
     // console.log(courses && courses[5]?.course?.courseCategory);
-
+    function formatDate(dateString) {
+        const dateObj = new Date(dateString);
+        
+        const day = String(dateObj.getDate()).padStart(2, '0');
+        const year = dateObj.getFullYear();
+    
+        const monthNames = [
+            "January", "February", "March", "April", "May", "June", 
+            "July", "August", "September", "October", "November", "December"
+        ];
+        const month = monthNames[dateObj.getMonth()];
+    
+        let hours = dateObj.getHours();
+        const minutes = String(dateObj.getMinutes()).padStart(2, '0');
+    
+        const ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        
+        const time = `${hours}.${minutes}${ampm}`;
+    
+        return `${day} ${month} ${year} ${time}`;
+    }
     return (<>
         {!courses?.length ? <div className="flex justify-center  w-full mt-10"><div className="text-center font-semibold text-2xl w-full "> No Course Purchased</div></div> : ''}
 
@@ -56,6 +78,7 @@ export default function Mycourse({ courses }) {
                         </div>
                         <div className="h-[45%] flex flex-col justify-between mt-2 xsm:space-y-1 xsm:mt-1">
                             <p className="font-pop font-semibold text-[18px] xsm:text-[6px] md:text-[16px]">{val?.course?.title}</p>
+                            <p className="text-gray-400 font-semibold text-sm">Batch will starts on {formatDate(val?.course?.courseStartDate)}</p>
                             <div className=" flex items-center gap-1">
                                 <img className="w-[32px] h-[32px] xsm:w-[10px] xsm:h-[10px] md:h-[24px] md:w-[24px] rounded-full" src={val?.course?.instructor?.profile} />
                                 <p className="font-pop font-medium text-[16px] xsm:text-[6px] md:text-[12px]"> {val?.course?.instructor?.name}</p>
