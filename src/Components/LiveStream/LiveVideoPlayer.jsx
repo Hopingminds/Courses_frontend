0b5ab2fb17fff4e652ffce6d39ad202ca3aa25c6
AnshulKaryal <karyalanshul@gmail.com>
2024-08-16 12:14:43 +0530

    Fixed re-render of video on typing

---
+++ src/Components/LiveStream/LiveVideoPlayer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactHlsPlayer from 'react-hls-player';
+
+const LiveVideoPlayer = ({ liveClassKey }) => {
+    return (
+        <ReactHlsPlayer
+            src={`https://stream.hopingminds.com/hls/${liveClassKey}.m3u8`}
+            autoPlay={false}
+            controls={true}
+            width="100%"
+            height="100%"
+            hlsConfig={{
+                maxLoadingDelay: 4,
+                minAutoBitrate: 0,
+                lowLatencyMode: true,
+            }}          
+        />
+    );
+};
+
+export default React.memo(LiveVideoPlayer);
