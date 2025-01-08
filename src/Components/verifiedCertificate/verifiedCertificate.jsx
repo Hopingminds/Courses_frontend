fb3d23c7d0cb76552b9b92acc0a87d600e0db3ce
Anshul Karyal <karyalanshul@gmail.com>
2025-01-08 11:06:22 +0530

    responsive VerifiedCertificate

--- src/Components/verifiedCertificate/verifiedCertificate.jsx
+++ src/Components/verifiedCertificate/verifiedCertificate.jsx
@@ -45,12 +45,11 @@ const VerifiedCertificate = () => {
         <div>
             {certificate ? (
                 <div className='flex flex-col items-center'>
-                    <img width="168" height="168" src="/ok.gif" alt="ok" />
-                    <p className="text-3xl text-gray-900 font-semibold mb-5 font-pop text-center">Your Certificate is a Verified Certificate From <br />HopingMinds</p>
+                    <img className='w-[168px] h-[168px] xsm:w-[100px] xsm:h-[100px] sm:w-[110px] sm:h-[110px] md:w-[130px] md:h-[130px]' src="/ok.gif" alt="ok" />
+                    <p className="text-3xl text-gray-900 font-semibold mb-5 font-pop text-center xsm:text-[16px] sm:text-[16px] md:text-[20px]">Your Certificate is a Verified Certificate From <br />HopingMinds</p>
                     <img
-                        className='mb-14 shadow-2xl'
+                        className='mb-14 shadow-2xl w-[50%] xsm:w-[80%] sm:w-[80%] md:w-[80%] '
                         src={certificate}
-                        width="50%"
                         title="Certificate"
                     />
                 </div>
