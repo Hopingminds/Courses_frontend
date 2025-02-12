6411a48aa4a82600a79ac586b740add74ee0fdba
Anshul Karyal <karyalanshul@gmail.com>
2025-02-12 12:17:36 +0530

    `Updated text in SubmittedSuccess component to reflect changes in assessment terminology.`

--- src/Components/Assignments/Submittedpage/submitted.js
+++ src/Components/Assignments/Submittedpage/submitted.js
@@ -8,7 +8,7 @@ function SubmittedSuccess() {
             <div className="thank-you">
                 <img width="188" height="188" src="/ok.gif" alt="ok" />
                 <h1 className='text-[#1DBF73] font-semibold font-pop'>Assessment Completed Successfully</h1>
-                <div className='text-center px-20'>Thank you for participating in PAP enrollment test. You will be receiving your test results via email in 24 Hrs.</div>
+                <div className='text-center px-20'>Thank you for participating in Assessment. You will be receiving your test results via email within 24 Hrs.</div>
                 <div className="button-container mt-5">
                     <a href={'/modules'}> <button className="view-order">Back to pap page</button></a>
                 </div>
